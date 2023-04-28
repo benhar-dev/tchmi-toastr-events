6eddc89d567a391ba2c3407e692e6ff69e91748f
Ben Harrison <53424421+benhar-dev@users.noreply.github.com>
2023-04-28 12:53:59 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Simple example of using toastr events in TwinCAT HMI
+
+## Disclaimer
+
+This is a personal guide not a peer reviewed journal or a sponsored publication. We make
+no representations as to accuracy, completeness, correctness, suitability, or validity of any
+information and will not be liable for any errors, omissions, or delays in this information or any
+losses injuries, or damages arising from its display or use. All information is provided on an as
+is basis. It is the reader’s responsibility to verify their own facts.
+
+The views and opinions expressed in this guide are those of the authors and do not
+necessarily reflect the official policy or position of any other agency, organization, employer or
+company. Assumptions made in the analysis are not reflective of the position of any entity
+other than the author(s) and, since we are critically thinking human beings, these views are
+always subject to change, revision, and rethinking at any time. Please do not hold us to them
+in perpetuity.
+
+## Overview
+
+This is a small example of using [toastr](https://github.com/CodeSeven/toastr) notifications in TwinCAT HMI coupled with the TcHmi.Server.Events.registerConsumer API.
+
+## Getting started
+
+Open TwinCAT HMI and switch to liveview (Although the code actually runs in development too)
+
+## Screenshot
+
+![image](./docs/images/Screenshot.png)
+
+## Versions
+
+- TcXaeShell 3.1.4024.44
+- TwinCAT HMI 1.12.758.8
+
+## Need more help?
+
+Please visit http://beckhoff.com/ for further guides
---
+++ src/tchmi-toast-events/Scripts/PopUpEventLogger.js
@@ -0,0 +1,51 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.12.748.0/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    // If you want to unregister an event outside the event code you need to use the return value of the method register()
+    var destroyOnInitialized = TcHmi.EventProvider.register('onInitialized', function (e, data) {
+        // This event will be raised only once, so we can free resources. 
+        // It's best practice to use destroy function of the event object within the callback function to avoid conflicts.
+        e.destroy();
+        // ----------------------
+        // Place your code here!
+        // ----------------------
+
+        TcHmi.Server.Events.registerConsumer(null,
+            {
+                subscription: function (data) {
+
+                    toastr.options = {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": true,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "200",
+                        "hideDuration": "1000",
+                        "timeOut": "3000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    }
+
+                    // check if the callback object is valid
+                    if (data.error === TcHmi.Errors.NONE) {
+                        // check if an event of type message is raised
+                        if (data.event.sourceDomain === "TcHmiSrv")
+                            return;
+
+                        toastr.info(data.event.text);
+
+                    }
+
+                }
+
+            }
+        );
+    });
+})(TcHmi);
